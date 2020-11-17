0018fb53a5faf68bcae6585f1d61c3fdb0b5c6ad
Clément Ollivier <clement.o2p@gmail.com>
2020-11-17 11:38:29 +0100

    :lipstick: Set base layout with tailwind

--- client/src/components/Card.vue
+++ client/src/components/Card.vue
@@ -1,12 +1,15 @@
 <template>
-  <div class="card">
+  <div class="card p-6 bg-white rounded-lg shadow-xl flex-1 h-full" @click="toggleFace" ref="card">
     <div class="card__inner">
-      <div class="card__front">
-        <h2 v-html="currentCard.A" />
+      <div class="card__front" v-show="!backVisible">
+        <h2 v-html="currentCard.A" class="text-center text-xl font-bold"/>
       </div>
-      <div class="card__back">
-        <h2 v-html="currentCard.A" />
+      <div class="card__back" v-show="backVisible">
+        <h2 class="text-center text-xl font-bold mb-2" v-html="currentCard.A" />
         <p v-html="currentCard.B" />
+      </div>
+      <div class="pt-6 pb-6">
+        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{{ currentCard.category }}</span>
       </div>
     </div>
   </div>
@@ -30,69 +33,13 @@ export default {
     toggleFace() {
       this.backVisible = !this.backVisible
     }
+  },
+  mounted() {
+    const links = this.$refs.card.querySelectorAll("a");
+    links.forEach(link => {
+      link.setAttribute("target", "_blank");
+      link.setAttribute("rel", "noreferrer noopener");
+    })
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-
-/* The flip card container - set the width and height to whatever you want. We have added the border property to demonstrate that the flip itself goes out of the box on hover (remove perspective if you don't want the 3D effect */
-.card {
-  background-color: transparent;
-  max-width: 80vw;
-  height: 80vh;
-  perspective: 1000px; /* Remove this if you don't want the 3D effect */
-  margin: 0 auto;
-}
-
-/* This container is needed to position the front and back side */
-.card__inner {
-  position: relative;
-  width: 100%;
-  height: 100%;
-  text-align: center;
-  transition: transform 0.6s;
-  transform-style: preserve-3d;
-}
-
-/* Do an horizontal flip when you move the mouse over the flip box container */
-.card:hover .card__inner {
-  transform: rotateY(180deg);
-}
-
-/* Position the front and back side */
-.card__front, .card__back {
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  -webkit-backface-visibility: hidden; /* Safari */
-  backface-visibility: hidden;
-}
-
-.card__front {
-  background-color: grey;
-  color: black;
-}
-
-/* Style the back side */
-.card__back {
-  background-color: salmon;
-  color: black;
-  transform: rotateY(180deg);
-}
-</style>
---
+++ client/src/components/Header.vue
@@ -0,0 +1,61 @@
+<template>
+<nav class="bg-gray-800 mb-4">
+  <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+    <div class="flex items-center justify-between h-16">
+      <h1 class="text-white font-bold">Flipcards.dev</h1>
+      <div class="flex items-center">
+        <div class="hidden md:block">
+          <div class="ml-10 flex items-baseline space-x-4">
+            <a href="#" class="px-3 py-2 rounded-md text-sm font-medium text-white bg-gray-900 focus:outline-none focus:text-white focus:bg-gray-700">Dashboard</a>
+          </div>
+        </div>
+      </div>
+      <div class="-mr-2 flex md:hidden">
+        <!-- Mobile menu button -->
+        <button @click="toggleOpen" class="inline-flex items-center justify-center p-2 rounded-full text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:bg-gray-700 focus:text-white">
+          <!-- Menu open: "hidden", Menu closed: "block" -->
+          <svg class="h-6 w-6 svg-icon" :class="isOpen ? 'hidden': 'block'" fill="none" viewBox="0 0 20 20">
+							<path d="M10,1.529c-4.679,0-8.471,3.792-8.471,8.471c0,4.68,3.792,8.471,8.471,8.471c4.68,0,8.471-3.791,8.471-8.471C18.471,5.321,14.68,1.529,10,1.529 M10,17.579c-4.18,0-7.579-3.399-7.579-7.579S5.82,2.421,10,2.421S17.579,5.82,17.579,10S14.18,17.579,10,17.579 M14.348,10c0,0.245-0.201,0.446-0.446,0.446h-5c-0.246,0-0.446-0.201-0.446-0.446s0.2-0.446,0.446-0.446h5C14.146,9.554,14.348,9.755,14.348,10 M14.348,12.675c0,0.245-0.201,0.446-0.446,0.446h-5c-0.246,0-0.446-0.201-0.446-0.446s0.2-0.445,0.446-0.445h5C14.146,12.229,14.348,12.43,14.348,12.675 M7.394,10c0,0.245-0.2,0.446-0.446,0.446H6.099c-0.245,0-0.446-0.201-0.446-0.446s0.201-0.446,0.446-0.446h0.849C7.194,9.554,7.394,9.755,7.394,10 M7.394,12.675c0,0.245-0.2,0.446-0.446,0.446H6.099c-0.245,0-0.446-0.201-0.446-0.446s0.201-0.445,0.446-0.445h0.849C7.194,12.229,7.394,12.43,7.394,12.675 M14.348,7.325c0,0.246-0.201,0.446-0.446,0.446h-5c-0.246,0-0.446-0.2-0.446-0.446c0-0.245,0.2-0.446,0.446-0.446h5C14.146,6.879,14.348,7.08,14.348,7.325 M7.394,7.325c0,0.246-0.2,0.446-0.446,0.446H6.099c-0.245,0-0.446-0.2-0.446-0.446c0-0.245,0.201-0.446,0.446-0.446h0.849C7.194,6.879,7.394,7.08,7.394,7.325"></path>
+          </svg>
+          <!-- Menu open: "block", Menu closed: "hidden" -->
+          <svg class="h-6 w-6 svg-icon" :class="isOpen ? 'block': 'hidden'" fill="none" viewBox="0 0 20 20">
+            <path d="M10.185,1.417c-4.741,0-8.583,3.842-8.583,8.583c0,4.74,3.842,8.582,8.583,8.582S18.768,14.74,18.768,10C18.768,5.259,14.926,1.417,10.185,1.417 M10.185,17.68c-4.235,0-7.679-3.445-7.679-7.68c0-4.235,3.444-7.679,7.679-7.679S17.864,5.765,17.864,10C17.864,14.234,14.42,17.68,10.185,17.68 M10.824,10l2.842-2.844c0.178-0.176,0.178-0.46,0-0.637c-0.177-0.178-0.461-0.178-0.637,0l-2.844,2.841L7.341,6.52c-0.176-0.178-0.46-0.178-0.637,0c-0.178,0.176-0.178,0.461,0,0.637L9.546,10l-2.841,2.844c-0.178,0.176-0.178,0.461,0,0.637c0.178,0.178,0.459,0.178,0.637,0l2.844-2.841l2.844,2.841c0.178,0.178,0.459,0.178,0.637,0c0.178-0.176,0.178-0.461,0-0.637L10.824,10z"></path>
+          </svg>
+        </button>
+      </div>
+    </div>
+  </div>
+
+  <!--
+    Mobile menu, toggle classes based on menu state.
+
+    Open: "block", closed: "hidden"
+  -->
+  <div class="md:hidden" :class="menuClass">
+    <div class="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+      <a href="#" class="block px-3 py-2 rounded-md text-base font-medium text-white bg-gray-900 focus:outline-none focus:text-white focus:bg-gray-700">Dashboard</a>
+    </div>
+  </div>
+</nav>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  data() {
+    return {
+      isOpen: false
+    }
+  },
+  computed: {
+    menuClass() {
+      return this.isOpen ? "block" : "hidden";
+    }
+  },
+  methods: {
+    toggleOpen() {
+      this.isOpen = !this.isOpen
+    }
+  }
+}
+</script>
