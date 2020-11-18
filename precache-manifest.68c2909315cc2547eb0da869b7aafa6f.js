ca2b4642bfd23595e5ae5734d5d33bdada25496d
Clément Ollivier <clement.o2p@gmail.com>
2020-11-18 17:23:40 +0100

    gh-pages

---
+++ precache-manifest.68c2909315cc2547eb0da869b7aafa6f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84e3ad88a4b315873ed5",
+    "url": "css/app.01f804d5.css"
+  },
+  {
+    "revision": "6d267c02cdeda26cc9ce",
+    "url": "css/chunk-vendors.dee901ad.css"
+  },
+  {
+    "revision": "61aaff0754d13200affd375735789717",
+    "url": "img/logo.svg"
+  },
+  {
+    "revision": "aecd9e03d0a1782848cb761983991848",
+    "url": "index.html"
+  },
+  {
+    "revision": "84e3ad88a4b315873ed5",
+    "url": "js/app.a897e342.js"
+  },
+  {
+    "revision": "6d267c02cdeda26cc9ce",
+    "url": "js/chunk-vendors.ca314e19.js"
+  },
+  {
+    "revision": "177f3b85b2283c070fd905e0398583e1",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
