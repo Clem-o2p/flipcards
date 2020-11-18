bdfb02d319560be29c304d851d1358003c9d8105
Clément Ollivier <clement.o2p@gmail.com>
2020-11-18 10:43:32 +0100

    :wrench: Add exceptions to purgeCSS to avoid stripping dynamic tags from markdown in CSS

--- client/postcss.config.js
+++ client/postcss.config.js
@@ -1,7 +1,16 @@
 module.exports = {
   plugins: {
     tailwindcss: {},
-    'vue-cli-plugin-tailwind/purgecss': {},
-    autoprefixer: {}
-  }
-}
+    "vue-cli-plugin-tailwind/purgecss": {
+      whitelistPatterns: [
+        /-(leave|enter|appear)(|-(to|from|active))$/,
+        /^(?!cursor-move).+-move$/,
+        /^router-link(|-exact)-active$/,
+        /^img$/,
+        /^code$/,
+        /^pre$/,
+      ],
+    },
+    autoprefixer: {},
+  },
+};
